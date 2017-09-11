0cd63a542755ec8c1cd42265fc5d1a4815a657da
Ted Dunn <tldunn451@gmail.com>
2017-09-11 11:52:50 -0700

    Add OrderManager and PriceManager to Domain  namespace

---
+++ PapaBobs.Domain/OrderManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaBobs.Domain
+{
+    public class OrderManager
+    {
+		public static void CreateOrder(DTO.OrderDTO dtoOrder)
+		{
+			dtoOrder.OrderId = Guid.NewGuid();
+			dtoOrder.TotalCost = PriceManager.CalculateTotalCost(dtoOrder);
+			Data.OrderRepository.CreateOrder(dtoOrder);
+		}
+
+		public static List<DTO.OrderDTO> GetOrders()
+		{
+			return Data.OrderRepository.GetOrders();
+		}
+
+		public static void CompleteOrder(Guid orderId)
+		{
+			Data.OrderRepository.CompleteOrder(orderId);
+		}
+	}
+}
